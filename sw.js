1438dac9779f1be57a2fb148f1238c22f4114ec2
Xtracker2232 <carter06530@gmail.com>
2026-06-09 18:40:44 +0200

    disable sw cache

--- sw.js
+++ sw.js
@@ -1,22 +1,7 @@
-const CACHE = 'xtracker-v3';
-
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.map(function(k) { return caches.delete(k); }));
-  }));
+// Service worker désactivé
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', function(e) {
-  // Tout passe par le réseau - pas de cache
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
-    })
-  );
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
